55c2b9703b6a7a008945dca209f39df97e15a4f0
Jara Santamaria <jarasmar@gmail.com>
2020-04-23 12:52:40 +0100

    create new file for refactored code. Add a code structure and method for updating common items (tests passing for this section. Brie and Backstage failing)

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,86 +1,46 @@
 require_relative 'item'
 
 class GildedRose
+
   def initialize(items)
     @items = items
   end
 
-  def update_quality
+  def update_quality()
     @items.each do |item|
-      # update quality
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            # if its not brie/backstage/sulfuras
-            # and quality > 0
-            # reduce it by 1
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          # if it is brie/backstage/
-          # and quality < 50
-          # increase it by 1
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                # if it is backstage
-                # quality < 50
-                # sell_in is 10-6 days
-                # increase it by extra 1 (1 + 1)
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                # if it is backstage
-                # quality < 50
-                # sell_in is 5-0 days
-                # increase it by extra 1 (1 + 1 + 1)
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-      # update sell_in date
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        # if its not Sulfuras
-        # decrease sell_in by 1
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                # if sell_in date is passed
-                # and item is not brie/backstage/sulfuras
-                # and quality is > 0
-                # decrease quality by extra 1 (1 + 1)
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            # if sell_in date is passed
-            # and item is backstage
-            # quality drops to 0
-            item.quality = item.quality - item.quality
-          end
+      if item != 'Sulfuras, Hand of Ragnaros'
+        if item == 'Aged Brie'
+          update_brie(item)
+        elsif item == 'Backstage passes to a TAFKAL80ETC concert'
+          update_backstage(item)
         else
-          if item.quality < 50
-            # if sell_in date is passed
-            # and item is brie
-            # and quality is < 50
-            # quality increases by extra 1 (1 + 1)
-            item.quality = item.quality + 1
-          end
+          update_item(item)
         end
       end
     end
+  end
+
+  def update_item(item)
+    if item.quality > 0
+      if item.sell_in > 0
+        item.quality -= 1
+      else
+        item.quality -= 2
+      end
+    end
+    item.sell_in -= 1
+  end
+
+  def update_brie(item)
+  #   - "Aged Brie": increases in Quality the older it gets
+  #   	- Quality increases by 1  before sell_in date
+  #   	- Quality increases by 2  after sell_in date
+  end
+
+  def update_backstage(item)
+    # - "Backstage passes": increases in Quality the older it gets:
+    # 	- Quality increases by 2 when there are 10 days or less
+    # 	- Quality increases by 3 when there are 5 days or less
+    # 	- Quality drops to 0 after the concert
   end
 end
