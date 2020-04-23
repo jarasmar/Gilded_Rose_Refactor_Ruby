10d0f598eb34b597fe8b486bd9f9f19fd43e74d0
Jara Santamaria <jarasmar@gmail.com>
2020-04-23 12:25:03 +0100

    add test for common items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -18,5 +18,26 @@ describe GildedRose do
       items = [Item.new("Item", 5, 5)]
       expect{ GildedRose.new(items).update_quality() }.to change{ items[0].sell_in }
     end
+
+    context 'common items' do
+
+      it 'before sell_in date quality degrades by 1' do
+        items = [Item.new('Item', 2, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 4
+      end
+
+      it 'after sell_in date quality degrades by 2' do
+        items = [Item.new('Item', -1, 5)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 3
+      end
+
+      it 'quality cant be below 0' do
+        items = [Item.new('Item', 3, 0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+    end
   end
 end
