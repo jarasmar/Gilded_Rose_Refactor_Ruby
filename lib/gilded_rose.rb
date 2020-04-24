57760d4c5cc46bd7b172632c3d4f08421f9b0f11
Jara Santamaria <jarasmar@gmail.com>
2020-04-24 10:32:23 +0100

    add new methods to avoid logic duplication and keep code DRY

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -27,12 +27,32 @@ class GildedRose
 
   private
 
+  def quality_positive(item)
+    item.quality > 0
+  end
+
+  def quality_below_max(item)
+    item.quality < 50
+  end
+
+  def sell_in_positive(item)
+    item.sell_in > 0
+  end
+
+  def more_than_10_days_left(item)
+    item.sell_in > 10
+  end
+
+  def less_than_6_days_left(item)
+    item.sell_in > 6
+  end
+
   def update_item(item)
     # Quality cant go below 0
     # Quality decreases by 1 before sell_in date
     # Quality decreases by 2 after sell_in date
-    if item.quality > 0
-      if item.sell_in > 0
+    if quality_positive(item)
+      if sell_in_positive(item)
         item.quality -= 1
       else
         item.quality -= 2
@@ -44,8 +64,8 @@ class GildedRose
     # Quality cant go over 50
     # Quality increases by 1  before sell_in date
     # Quality increases by 2  after sell_in date
-    if item.quality < 50
-      if item.sell_in > 0
+    if quality_below_max(item)
+      if sell_in_positive(item)
         item.quality += 1
       else
         item.quality += 2
@@ -59,12 +79,12 @@ class GildedRose
     # Quality increases by 2 when there are 10 days or less
     # Quality increases by 3 when there are 5 days or less
     # Quality drops to 0 after the concert
-    if item.quality < 50
-      if item.sell_in > 10
+    if quality_below_max(item)
+      if more_than_10_days_left(item)
         item.quality += 1
-      elsif item.sell_in > 6
+      elsif less_than_6_days_left(item)
         item.quality += 2
-      elsif item.sell_in > 0
+      elsif sell_in_positive(item)
         item.quality += 3
       else
         item.quality -= item.quality
@@ -76,8 +96,8 @@ class GildedRose
     # Quality cant go below 0
     # Quality decreases by 2 before sell_in date
     # Quality decreases by 4 after sell_in date
-    if item.quality > 0
-      if item.sell_in > 0
+    if quality_positive(item)
+      if sell_in_positive(item)
         item.quality -= 2
       else
         item.quality -= 4
